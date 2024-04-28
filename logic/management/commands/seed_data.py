72ebfc4f020993a8f88dbd791134ce4edb2ae592
Dh-Kh <vsevolod.zaychenko@gmail.com>
2024-04-28 17:33:50 +0300

    tree update

--- logic/management/commands/seed_data.py
+++ logic/management/commands/seed_data.py
@@ -4,25 +4,19 @@ from django_seed import Seed
 from logic.models import EmployeeModel
 
 class Command(BaseCommand):
-    help = 'Seed the database with fake data for EmployeeModel'
+    help = 'Database seeding command'
     
     def handle(self, *args, **options):
         seeder = Seed.seeder()
-        seeder.add_entity(EmployeeModel, 50000, {
-            'full_name': lambda x: seeder.faker.name(),
-            'position': lambda x: random.choice([level[0] for level in EmployeeModel.HIERARCHY]),
-            'joined': lambda x: seeder.faker.date_this_decade(),
-            'email': lambda x: seeder.faker.email(),
-        })
-        inserted_pks = seeder.execute()
-        for boss_pk in inserted_pks[EmployeeModel]:
-            boss = EmployeeModel.objects.get(pk=boss_pk)
-            num_subordinates = random.randint(0, 10)  
-            subordinate_pks = random.sample(inserted_pks[EmployeeModel], num_subordinates)
-            for subordinate_pk in subordinate_pks:
-                subordinate = EmployeeModel.objects.get(pk=subordinate_pk)
-                subordinate.boss = boss
-                subordinate.save()
-
-        self.stdout.write(self.style.SUCCESS('Database successfully seeded with fake data.'))
+        get = lambda node_id: EmployeeModel.objects.get(pk=node_id)
+        root = EmployeeModel.add_root(
+            full_name=seeder.faker.name(),
+            position=8,
+            joined=seeder.faker.date_this_decade(),
+            email = seeder.faker.email()
+        )
+        position_level = 7
+        level = 50000//7
         
+        
+        self.stdout.write(self.style.SUCCESS('Completed!'))
--- logic/models.py
+++ logic/models.py
@@ -1,9 +1,19 @@
 from django.db import models
-from django.core.exceptions import ValidationError
+from django.db.models import CheckConstraint, Q, F
+from treebeard.mp_tree import MP_Node
 
-
-class EmployeeModel(models.Model):
+class EmployeeModel(MP_Node):
+    
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                check=~Q(parent__position__gte=F("position")),
+                name="parent_position_check"
+                )
+            ]
+    
     HIERARCHY = (
+        (8, "Owner"),
         (7, "SEO"),
         (6, "Team Lead"),
         (5, "Tech Lead"),
@@ -11,21 +21,11 @@ class EmployeeModel(models.Model):
         (3, "Middle"),
         (2, "Junior"),
         (1, "Trainee"),
-        )
+    )
+    
     full_name = models.CharField(max_length=255) 
     position = models.IntegerField(choices=HIERARCHY)
     joined = models.DateField()
     email = models.EmailField()
-    boss = models.ForeignKey("self", on_delete=models.CASCADE, 
-                             null=True, blank=True, related_name="subordinates")
     
-    def clean(self):
-        if self.boss and self.position > self.boss.position:
-            raise ValidationError('Boss position must be higher')
-        
-    def hierarchy_tree(self):
-        return {
-            "employee": self,
-            'subordinates': [
-                subordinates.hierarchy_tree() for subordinates in self.subordinates.all()]
-            } if self.subordinates.exists() else {'employee': self, 'subordinates': []}
+    node_order_by = ["-position"]
